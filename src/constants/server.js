8c3a088a0c6966c2583d9844cef71ca221a71195
ritik72199 <119751590+ritik72199@users.noreply.github.com>
2023-05-06 21:00:19 +0530

    master form

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://tolfa.arihantchemical.in/api/v1";
-export const BASE_URL = "http://localhost:47381/api/v1";
+// export const BASE_URL = "http://localhost:47381/api/v1";
+export const BASE_URL = "https://tolfa-server.onrender.com/api/v1";
